efba98ad23d7551fd78c9fd1f6bd5179088ea5cf
dimsmith <dimsmith.dev@gmail.com>
2021-02-09 22:33:11 +0700

    Init

---
+++ src/main/kotlin/com/github/dimsmith/restspringtemplate/RestSpringTemplateApplication.kt
@@ -0,0 +1,11 @@
+package com.github.dimsmith.restspringtemplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestSpringTemplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestSpringTemplateApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/dimsmith/restspringtemplate/RestSpringTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.dimsmith.restspringtemplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestSpringTemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
